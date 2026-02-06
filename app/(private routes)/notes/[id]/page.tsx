5236789e9eea1953a5e4e0a3d63c685f423a01c9
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-02-06 19:04:39 +0200

    fix(routing): add notes/[id] route and fix app structure

---
+++ app/(private routes)/notes/[id]/page.tsx
@@ -0,0 +1,37 @@
+import { cookies } from 'next/headers';
+import { HydrationBoundary, dehydrate, QueryClient } from '@tanstack/react-query';
+import { notFound } from 'next/navigation';
+import NotePreviewClient from '@/app/@modal/(.)notes/[id]/NotePreview.client';
+
+import { fetchNoteById } from '@/lib/api/serverApi';
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function NotePreviewPage({ params }: Props) {
+  const { id } = await params;
+
+  const queryClient = new QueryClient();
+
+  const cookieStore = await cookies();
+  const cookie = cookieStore
+    .getAll()
+    .map(({ name, value }) => `${name}=${value}`)
+    .join('; ');
+
+  try {
+    await queryClient.prefetchQuery({
+      queryKey: ['note', id],
+      queryFn: () => fetchNoteById(id, cookie),
+    });
+  } catch {
+    notFound();
+  }
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotePreviewClient id={id} />
+    </HydrationBoundary>
+  );
+}
